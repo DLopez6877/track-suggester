45d252afe2f1120d3d96db0bebaf78eef6fb728c
Daniel Lopez <dlopez6877@gmail.com>
2017-04-28 12:37:15 -0700

    add branches for results

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
     event.preventDefault();
     $(this).fadeOut();
     $(this).parent().next().fadeIn(2500);
+    $("#result-css").hide();
 
   });
 
@@ -31,12 +32,27 @@ $(document).ready(function() {
     var preferInput = parseInt($("#prefer").val());
     var interactiveInput = parseInt($("#interactive").val());
     var question1Input = parseInt($("#question1").val());
-    var score = add(preferInput, interactiveInput, question1Input)
+    var score = add(preferInput, interactiveInput, question1Input);
 
     $(this).fadeOut();
-    $(".suggestion").text(score);
-    $("#result").fadeIn(2500);
-      console.log(preferInput, interactiveInput, question1Input)
+
+    if ( score < 4 ) {
+      $("#result-css").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input, score);
+    } else if ( score < 7 && score > 3 ){
+      $("#result-ruby").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input, score);
+    } else if ( score < 10 && score > 6 ){
+      $("#result-php").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input, score);
+    } else if ( score < 13 && score > 9 ){
+      $("#result-java").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input, score);
+    } else {
+      $("#result-cnet").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input, score);
+    }
+
   });
 
 });
