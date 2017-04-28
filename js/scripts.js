3752e037e87b8503cf40983e8a2f776a6bd8463e
Daniel Lopez <dlopez6877@gmail.com>
2017-04-28 11:26:05 -0700

    add jquery variables for prefer interactive question

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,39 @@
 // This is the business (or back-end) logic:
 
-
+var add = function(preferInput, interactiveInput, question1Input) {
+  return preferInput + interactiveInput + question1Input;
+}
 
 // Everything below this line is the user interface (or front-end) logic:
 
 $(document).ready(function() {
 
+  var preferInput = parseInt($("#prefer").val());
+  var interactiveInput = parseInt($("#interactive").val());
+  var question1Input = parseInt($("#question1").val());
+
   $("#begin").click(function() {
     $(this).children().fadeOut();
     $(this).next().fadeIn(2500);
+
   });
 
   $("form").submit(function(event) {
     event.preventDefault();
+
+    console.log(preferInput, interactiveInput, question1Input)
+
     $(this).fadeOut();
     $(this).parent().next().fadeIn(2500);
+
   });
 
   $("form#final-form").submit(function(event) {
     event.preventDefault();
+
     $(this).fadeOut();
     $("#result").fadeIn(2500);
+      console.log(preferInput, interactiveInput, question1Input)
   });
 
 });
